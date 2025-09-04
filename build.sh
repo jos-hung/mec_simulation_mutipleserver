df3aca41211e36294d29820d85a050b9a7f22b05
Ngoc Hung Nguyen <ngochung.nguyenlg@gmail.com>
2025-09-04 08:15:57 +0200

    fix errors port

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ docker build -f docker/Dockerfile -t mec_simulation:latest .
 
 BASE_PORT=10000
 
-for i in {1..1}; do
+for i in {1..2}; do
     container_name="mec_simulation_$i"
     port=$((BASE_PORT + i))  
 
     echo "Starting container $container_name on port $port ..."
 
     docker run -dit \
-        --network=host \
-        -v $(pwd)/val2017:/shared \
-        -v $(pwd)/service:/service \
+        -p $port:$port \
+        -v ./val2017:/shared \
+        -v ./service:/service \
         --name $container_name \
         mec_simulation:latest \
         
@@ -21,22 +21,21 @@ for i in {1..1}; do
     echo "Sending install request to container $i ..."
 done
 wait
-for i in {1..1}; do
+for i in {1..2}; do
     container_name="mec_simulation_$i"
     port=$((BASE_PORT + i))
     echo "Starting uvicorn inside container $container_name on port $port ..."
-    osascript -e "tell application \"Terminal\" to do script \"docker exec -it $container_name bash -c 'PYTHONPATH=src uvicorn handle_host_request:app --host 0.0.0.0 --port $port'\""
+    osascript -e "tell application \"Terminal\" to do script \"docker exec -it $container_name bash -c 'cd src && uvicorn handle_host_request:app --host 0.0.0.0 --port $port'\""
     # gnome-terminal -- bash -c "docker exec -it $container_name bash -c 'PYTHONPATH=src uvicorn handle_host_request:app --host 0.0.0.0 --port $port'; exec bash"
 done
 wait
-for i in {1..1}; do
+for i in {1..2}; do
     port=$((BASE_PORT + i))
     echo "Sending install request to docker $i on port $port ..."
     .venv/bin/python3 host_send_request.py \
         --request install \
         --num 1 \
         --docker $i \
-        --host 0.0.0.0 \
         --port $port
 done
 wait
