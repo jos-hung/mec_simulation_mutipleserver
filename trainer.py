4cb9a67e6f8b73d1a5441be4593010f7ecd0dbcf
nguyen ngoc hung <nnhungbk@gmail.com>
2025-09-22 14:14:44 +0700

    update code

--- trainer.py
+++ trainer.py
@@ -5,6 +5,8 @@ import re, sys, subprocess
 from env import OffloadingEnv
 from ddqn_agent import DDQNAgent
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg") 
 import time
 from utils import get_active_service, get_feature
 import os
@@ -16,7 +18,7 @@ N_SERVER = 4
 df = {"id": [], "id_picture":[], "predict_cost": [] }
 experiment_types = ["random", "drl_train", "drl_prediction", "esimated_processing_time"]
 
-def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000, output_file = "results", N_SERVER = 4, experiment_type = 0, LGOBAL_SEED = 45):
+async def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000, output_file = "results", N_SERVER = 4, experiment_type = 0, LGOBAL_SEED = 45):
     list_docker, list_service_in_docker = get_active_service()
     output_file = output_file + f"_{len(list_service_in_docker[1])}"
     os.makedirs(output_file, exist_ok=True)
@@ -34,17 +36,18 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
     system_arrival_rate = n_users * lamd
     system_inter_arrival_rate = 1 / system_arrival_rate
     cnt = 0
-    agent = DDQNAgent(28, N_SERVER)
-    if experiment_types[experiment_type] == 'drl_prediction':
-        agent.load()
+    
 
     # vì OffloadingEnv có async nên ta tạo 1 loop riêng trong thread
-    env = asyncio.run(OffloadingEnv(num_servers=4).ainit()) if hasattr(OffloadingEnv, "ainit") else OffloadingEnv(num_servers=3)
-    obs = asyncio.run(env.get_observation())
+    env = OffloadingEnv(num_servers=N_SERVER)
+    await env.ainit()
+    obs = await env.get_observation()
     queue = {}
     rewards = []
     all_reward = {}
-
+    agent = DDQNAgent(len(obs), N_SERVER)
+    if experiment_types[experiment_type] == 'drl_prediction':
+        agent.load()
     done = False
 
     save_dir = "train_result"
@@ -55,11 +58,8 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
     check_done  = 0
     while duration > 0:
         event = rng.exponential(system_inter_arrival_rate)
-        time_sleep = event if event > 0 else 0
-        # ngủ sync
-        time.sleep(time_sleep)
-
-        # chọn docker
+        print(event)
+        await asyncio.sleep(event)
         id_picture = rng.integers(0, len(os.listdir("val2017")))
 
         model = rng.choice(list_service_in_docker[1])
@@ -71,11 +71,12 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
             model_id = 0
             if model =="ssd":
                 model_id = 9
+            elif model =="resnet34":
+                model_id = 3
             start_time = time.perf_counter()
             def select_server(obs):
                 slected_docker = 0            
                 min_processing_predicted_time = float('inf')
-
                 for i in range(N_SERVER):
                     fearture_vecs = np.array(get_feature(obs, id_picture, model_id, i+1)) #id docker from 1
                     fearture_vecs = np.reshape(fearture_vecs, (1, -1))
@@ -89,12 +90,8 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
             df["id"].append(cnt)
             df["id_picture"].append(id_picture)
             df['predict_cost'].append(predict_cost)
-
             slected_docker = select_server(obs)
-
         slected_port = port_base + slected_docker
-
-
         cmd = [
             sys.executable,
             "host_send_request.py",
@@ -107,14 +104,13 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
             "--model", str(model),
             "--id_picture", str(id_picture),
         ]
-        
         subprocess.Popen(cmd)
         # gọi các async trong thread
-        next_state = asyncio.run(env.get_observation())
+        next_state = await env.get_observation()
         # next_state = obs
         queue[cnt] = [obs.copy(), slected_docker - 1, next_state]
         obs = next_state
-        reward = asyncio.run(env.get_reward())
+        reward = await env.get_reward()
         all_reward.update(reward)
 
         #====================
@@ -135,7 +131,7 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
                     if re_val != "None" and re_val is not None:
                         train_data = queue[int(taskid)]
                         train_data.append(-float(re_val))
-                        if re_val > 10 or (cnt > 0 and cnt%200 == 0 and experiment_types[experiment_type] == 'drl_train'):
+                        if re_val > 10 or (cnt > 0 and cnt%100 == 0 and experiment_types[experiment_type] == 'drl_train'):
                             done = True
                             agent.remember(train_data[0], train_data[1], train_data[2], train_data[3], True)
                         else:
@@ -153,14 +149,16 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
             for k in del_r_key:
                 del all_reward[k]
             agent.update()
-        asyncio.run(asyncio.to_thread(process_rewards))
+
+        await asyncio.to_thread(process_rewards)
+
         #====================
         # if check_done >100:
         #     done = True
         #     check_done=0
         while done:
             #sau khi done thì vẫn còn các nhiệm vụ trong queue, phải chờ cho chúng kết thúc rồi ms chuyển qua epoch mới
-            asyncio.run(asyncio.to_thread(process_rewards))
+            await asyncio.to_thread(process_rewards)
         done = False
         if cnt > 0 and cnt % 100 == 0:
             plt.plot(rewards)
@@ -178,9 +176,6 @@ def run(n_users=10, lamd=1.1, port_base=10000, docker_min_max=[], duration=1000,
                 print(r.status_code, r.text)
             except Exception as e:
                 print(f"cannot save file {e} existing...")
-        if cnt > 0 and cnt % 1000 == 0 and experiment_types[experiment_type] =='drl_train':
-            agent.update_target()
-            print("update target model succes!!!")
         check_done += 1
 
         duration -= event
@@ -199,6 +194,7 @@ if __name__ == "__main__":
     ap.add_argument("--LGOBAL_SEED", type=int, required=False)
     
     args = ap.parse_args()
+    asyncio.run(
     run(n_users=args.n_users,
         lamd= args.lamd,
         docker_min_max=args.docker_min_max,
@@ -206,7 +202,7 @@ if __name__ == "__main__":
         output_file=args.output_file,
         experiment_type=args.experiment_type,
         LGOBAL_SEED=args.LGOBAL_SEED
-        )
+        ))
     df = pd.DataFrame(df)
     df.to_csv(f"{args.output_file}/{experiment_types[int(args.experiment_type)]}_n_user_{args.n_users}_lamda_{args.lamd}_cost.csv", index=None)
 
